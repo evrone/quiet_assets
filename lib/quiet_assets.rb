c73c869a32b787ae6efc98be9bfe92ab623449aa
Dmitriy Vorotilin <mydeeptown@gmail.com>
2012-03-22 01:36:02 +0400

    Parse incoming req by assets prefix

--- lib/quiet_assets.rb
+++ lib/quiet_assets.rb
@@ -7,6 +7,8 @@ module QuietAssets
 
     initializer "quiet_assets", :after => "sprockets.environment" do |app|
       if app.config.quiet_assets == true
+        # Parse PATH_INFO by assets prefix
+        ASSETS_PREFIX = app.config.assets.prefix.split("/").join("/") + "/"
         app.config.assets.logger = false
 
         # Just create an alias for call in middleware
@@ -14,13 +16,12 @@ module QuietAssets
           def call_with_quiet_assets(env)
             old_logger_level, level = Rails.logger.level, Logger::ERROR
             # Increase log level because of messages that have a low level should not be displayed
-            Rails.logger.level = level if env['PATH_INFO'].index("/assets/") == 0
+            Rails.logger.level = level if env['PATH_INFO'].index(ASSETS_PREFIX) == 0
             call_without_quiet_assets(env)
           ensure
             # Return back
             Rails.logger.level = old_logger_level
           end
-
           alias_method_chain :call, :quiet_assets
         end
       end
