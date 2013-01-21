48d16a2f9799740feaef3af392f5fac85cb9f80f
Dmitry Vorotilin <d.vorotilin@gmail.com>
2013-01-21 12:04:57 +0400

    Using middleware instead of aliasing

--- lib/quiet_assets.rb
+++ lib/quiet_assets.rb
@@ -1,27 +1,33 @@
 module QuietAssets
+  class Middleware
+    def initialize(app, assets_prefix)
+      @app, @assets_prefix = app, assets_prefix
+    end
+
+    def call(env)
+      if env['PATH_INFO'].start_with?(@assets_prefix)
+        # Skip logger middleware if request is for assets.
+        @app.instance_variable_get(:@app).call(env)
+      else
+        @app.call(env)
+      end
+    end
+  end
+
   class Engine < ::Rails::Engine
     # Set as true but user can override it
     config.quiet_assets = true
 
-    initializer 'quiet_assets', :after => 'sprockets.environment' do |app|
+    initializer 'quiet_assets' do |app|
       next unless app.config.quiet_assets
-      # Parse PATH_INFO by assets prefix
-      ASSETS_PREFIX = "/#{app.config.assets.prefix[/\A\/?(.*?)\/?\z/, 1]}/"
+
       app.config.assets.logger = false
 
-      # Just create an alias for call in middleware
-      Rails::Rack::Logger.class_eval do
-        def call_with_quiet_assets(env)
-          old_logger_level, level = Rails.logger.level, Logger::ERROR
-          # Increase log level because of messages that have a low level should not be displayed
-          Rails.logger.level = level if env['PATH_INFO'].start_with?(ASSETS_PREFIX)
-          call_without_quiet_assets(env)
-        ensure
-          # Return back
-          Rails.logger.level = old_logger_level
-        end
-        alias_method_chain :call, :quiet_assets
-      end
+      # Parse PATH_INFO by assets prefix
+      assets_prefix = "/#{app.config.assets.prefix[/\A\/?(.*?)\/?\z/, 1]}/"
+      app.middleware.insert_before(
+        Rails::Rack::Logger, Middleware, assets_prefix
+      )
     end
   end
 end
